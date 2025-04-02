c28f290a14f5040a820d1c0e12c29301fabea0c0
pavelleonenko007 <pavel.leonenko374@gmail.com>
2025-04-02 17:47:16 +0300

    fix: replace thevogne style.css file

--- header_block.php
+++ header_block.php
@@ -7,7 +7,6 @@
 		<script type="text/javascript">WebFont.load({  google: {    families: ["Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"]  }});</script>
 		<script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 		<link href="https://thevogne.ru/clients/creativepeople/bsqr/main.css" rel="stylesheet">
-		<link href="https://thevogne.ru/clients/creativepeople/bsqr/style.css" rel="stylesheet">
 		<!-- <script defer src="https://thevogne.ru/clients/creativepeople/bsqr/app.js"></script> -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
 		<style>
@@ -30,22 +29,5 @@ var query_vars =
 		<?php wp_head(); ?>
 		<?php if(function_exists('get_field')) { echo get_field('head_code', 'option'); } ?>
 		<?php if(file_exists(dirname( __FILE__ ).'/header_code.php')){ include_once 'header_code.php'; } ?>
-	
-	<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(98685272, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/98685272" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
 	</head>
